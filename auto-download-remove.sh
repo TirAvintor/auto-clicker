a51ef4ecce010daae262cd5749cc24d98741594d
Tir Avintor <TirAvintor@hotmail.com>
2026-01-16 10:29:10 +0200

    Update auto-download-remove.sh

--- auto-download-remove.sh
+++ auto-download-remove.sh
@@ -27,6 +27,26 @@ sleep 2
 # Pressing Enter
 xdotool key Return
 echo "Pressed Enter"
+sleep 20
+
+# Entering the command "uptime"
+xdotool type "uptime"
+echo "Entered: uptime"
+sleep 2
+
+# Pressing Enter
+xdotool key Return
+echo "Pressed Enter"
+sleep 10
+
+# Entering the command "clear"
+xdotool type "clear"
+echo "Entered: clear"
+sleep 2
+
+# Pressing Enter
+xdotool key Return
+echo "Pressed Enter"
 
 echo "Loop complete. Repeating..."
 done
