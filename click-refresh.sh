5ad31967362e3ca3b36c499a2c00605ee45a2266
Tir Avintor <TirAvintor@hotmail.com>
2026-01-16 09:12:23 +0200

    Update click-refresh.sh

--- click-refresh.sh
+++ click-refresh.sh
@@ -2,8 +2,8 @@
 # sudo apt install xdotool
 
 while true; do
-echo "Waiting 50 seconds..."
-sleep 50
+echo "Waiting 60 seconds..."
+sleep 60
 
 echo "Right click on (243, 236)"
 xdotool mousemove 243 236 click 3
@@ -19,21 +19,17 @@ sleep 2
 
 echo "Left click on (941, 459)"
 xdotool mousemove 941 459 click 1
-sleep 10
-
-echo "Left click on (301, 169)"
-xdotool mousemove 301 169 click 1
-sleep 10
 
 # refresh website
 
+sleep 180
 echo "Left click on (93, 105)"
 xdotool mousemove 93 105 click 1
-sleep 20
+sleep 30
 
 echo "Left click on (852, 383)"
 xdotool mousemove 852 383 click 1
-sleep 20
+sleep 30
 
 echo "Left click on (1442, 201)"
 xdotool mousemove 1442 201 click 1
