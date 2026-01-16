1dcba1143352c4c806201e63b16ca5db8104aa9c
Tir Avintor <TirAvintor@hotmail.com>
2026-01-16 09:46:23 +0200

    Create auto-download-remove.sh

---
+++ auto-download-remove.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# sudo apt install xdotool
+
+while true; do
+echo "Waiting 60 seconds..."
+sleep 60
+
+echo "Left click on (1017, 466)"
+xdotool mousemove 1017 466 click 1
+sleep 2
+
+# Entering the command "apt install sl -y"
+xdotool type "apt install sl -y"
+echo "Entered: apt install sl -y"
+sleep 2
+
+# Pressing Enter
+xdotool key Return
+echo "Pressed Enter"
+sleep 30
+
+# Entering the command "apt remove sl -y"
+xdotool type "apt remove sl -y"
+echo "Entered: apt remove sl -y"
+sleep 2
+
+# Pressing Enter
+xdotool key Return
+echo "Pressed Enter"
+
+echo "Loop complete. Repeating..."
+done
